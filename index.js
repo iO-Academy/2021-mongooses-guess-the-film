76d7bdbf7fe3d0415f42ec09442c197b17f42d57
Freddie Reschwamm <fredreschwamm@outlook.com>
2021-08-17 16:44:08 +0100

    tidied up submit button and removed css

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let submit_button = document.getElementById('submit_button')
-let input = document.getElementById('guess')
-let answer = document.getElementById('answer')
+let guess = document.getElementById('guess')
+let answer_element = document.getElementById('answer')
+let answer = answer_element.textContent
 let incorrect_response = document.getElementById('incorrect_response')
 let correct_response = document.getElementById('correct_response')
 
+answer_element.hidden = true;
+incorrect_response.hidden = true;
+correct_response.hidden = true;
+
 submit_button.addEventListener("click", function () {
-    let player_guess = input.value
+    incorrect_response.hidden = true;
+    correct_response.hidden = true;
+    let player_guess = guess.value
     if(player_guess.toLowerCase() === answer.toLowerCase()) {
-        correct_response.style.display="block"
+        correct_response.hidden = false
     } else {
-        incorrect_response.style.display="block"
+        incorrect_response.hidden = false;
     }
-    input.value = ''
+    guess.value = ''
 })
 
 
 
 
-// if(!player_guess.match(/^[A-Za-z\s-!?:'"]+$/)) {
