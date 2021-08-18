b836c0db9898706d0577dc46d10ab10661b5b2c3
Stuart McCann <smccann.software@gmail.com>
2021-08-18 13:09:31 +0100

    Tidied inconsistent quotation marks

--- index.js
+++ index.js
@@ -17,40 +17,40 @@ function get_new_question() {
         let quote_element = document.getElementById('quote')
         quote_element.textContent = current_movie.quote
         let answer_element = document.getElementById('answer')
-        answer_element.style.display = "none"
+        answer_element.style.display = 'none'
         answer_element.innerHTML = '<h2>' + current_movie.title + '</h2>'
         let correct_incorrect_elem = document.getElementById('correct_incorrect')
-        correct_incorrect_elem.style.display = "none"
+        correct_incorrect_elem.style.display = 'none'
     }
 }
 
 let remaining_movies = {}
-document.getElementById('submit_button').addEventListener("click", function () {
+document.getElementById('submit_button').addEventListener('click', function () {
     let guess = document.getElementById('guess')
     let player_guess = guess.value
     let answer = document.getElementById('answer').textContent
     let correct_response = document.getElementById('correct_response')
     let incorrect_response = document.getElementById('incorrect_response')
     if(player_guess.toLowerCase() === answer.toLowerCase()) {
-        correct_response.style.display = "block"
-        incorrect_response.style.display = "none"
+        correct_response.style.display = 'block'
+        incorrect_response.style.display = 'none'
         guess.value = ''
         document.getElementById('next_question_button').disabled = false
     } else {
-        correct_response.style.display = "none"
-        incorrect_response.style.display = "block"
+        correct_response.style.display = 'none'
+        incorrect_response.style.display = 'block'
     }
     let correct_incorrect_elem = document.getElementById('correct_incorrect')
-    correct_incorrect_elem.style.display = "block"
+    correct_incorrect_elem.style.display = 'block'
     let user_feedback_elem = document.getElementById('user_feedback')
-    user_feedback_elem.style.display = "block"
+    user_feedback_elem.style.display = 'block'
 })
 
 document.getElementById('start_button').addEventListener('click', (e) => {
-    document.getElementById('start_screen').style.display = "none"
-    document.getElementById('game_screen').style.display = "block"
+    document.getElementById('start_screen').style.display = 'none'
+    document.getElementById('game_screen').style.display = 'block'
     document.getElementById('next_question_button').disabled = true
-    fetch("src/films.json")
+    fetch('src/films.json')
         .then(data => data.json())
         .then((data) => {
             remaining_movies = shuffle_array(data.films)
